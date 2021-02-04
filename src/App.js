1c23ad08cccecbeaf480b6c3dfdc738a5a8dfaef
devatearth <64774809+devatearth@users.noreply.github.com>
2021-02-04 12:20:49 +0530

    Created ‘Header’ Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-function App() {
-    return ( <
-        div >
-        Phone Directrory <
-        /div>
+import React, { Component } from "react";
+import Header  from "./Header.js";
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <button> Add </button>{" "}
+        <div>
+          <span> Name </span> <br />
+          <span> Phone </span>{" "}
+        </div>{" "}
+      </div>
     );
+  }
 }
 
 export default App;
