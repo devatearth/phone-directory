295663447e361a460162957ce97ce0c2ffd2b8bc
devatearth <64774809+devatearth@users.noreply.github.com>
2021-02-07 22:22:38 +0530

    Added routing for Add Subscriber page and Show Subscribers page

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import "./AddSubscriber.css";
+import { Link } from "react-router-dom";
 
 class AddSusbscriber extends Component {
   constructor() {
@@ -20,29 +21,50 @@ class AddSusbscriber extends Component {
   onFormSubmitted = (e) => {
     e.preventDefault();
     this.props.addSubscriberHandler(this.state);
-    this.setState({ id: 0, name: '', phone: ' ' });
-}
+    this.setState({ id: 0, name: "", phone: " " });
+    this.props.history.push("/");
+  };
   render() {
     const { name, phone } = this.state;
     return (
       <div>
         <Header heading="Add Subscriber" />
         <div className="component-body-container">
-          <button className="custom-btn">Back</button>
-
-          <form className="subscriber-form" onSubmit={this.onFormSubmitted.bind(this)}>
+          <Link to="/">
+            <button className="custom-btn">Back</button>
+          </Link>
+          <form
+            className="subscriber-form"
+            onSubmit={this.onFormSubmitted.bind(this)}
+          >
             <label htmlFor="name" className="label-control">
               Name:{" "}
             </label>
             <br />
-            <input id="name" type="text" className="input-control" name="name" onChange={this.inputChangedHandler}/><br/><br/>
+            <input
+              id="name"
+              type="text"
+              className="input-control"
+              name="name"
+              onChange={this.inputChangedHandler}
+            />
+            <br />
+            <br />
             <br />
             <br />
             <label htmlFor="phone" className="label-control">
               Phone:{" "}
             </label>
             <br />
-            <input id="phone" type="text" className="input-control" name="phone" onChange={this.inputChangedHandler}/><br/><br/>
+            <input
+              id="phone"
+              type="text"
+              className="input-control"
+              name="phone"
+              onChange={this.inputChangedHandler}
+            />
+            <br />
+            <br />
             <br />
             <br />
 
--- src/PhoneDirectory.js
+++ src/PhoneDirectory.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import AddSubscriber from "./AddSubscriber";
 import ShowSubscribers from "./ShowSubscribers";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class PhoneDirectory extends Component {
   constructor() {
@@ -12,7 +13,7 @@ class PhoneDirectory extends Component {
   addSubscriberHandler = (newSubscriber) => {
     let subscribersList = this.state.subscribersList;
     if (subscribersList.length > 0) {
-      newSubscriber.id = subscribersList[subscribersList.length].id + 1;
+      newSubscriber.id = subscribersList[subscribersList.length - 1].id + 1;
     } else {
       newSubscriber.id = 1;
     }
@@ -20,8 +21,33 @@ class PhoneDirectory extends Component {
     this.setState({ subscribersList: subscribersList });
   };
   render() {
-    //return <AddSubscriber addSubscriberHandler = {this.addSubscriberHandler}/>;
-    return <ShowSubscribers subscribersList={this.state.subscribersList} />;
+    return (
+      <Router>
+        <div>
+          <Route
+            exact
+            path="/"
+            render={(props) => (
+              <ShowSubscribers
+                {...props}
+                subscribersList={this.state.subscribersList}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/add"
+            render={({ history }, props) => (
+              <AddSubscriber
+                history={history}
+                {...props}
+                addSubscriberHandler={this.addSubscriberHandler}
+              />
+            )}
+          />
+        </div>
+      </Router>
+    );
   }
 }
 
